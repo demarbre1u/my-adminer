c51e749b34bbb53b313e3f6ead49c0e250f14c0e
Allan Demarbre <ademarbre@groupe-cac.com>
2023-11-28 11:10:27 +0100

    Ajout d'un bouton pour beautify les requêtes SQL

--- plugins/autocomplete.php
+++ plugins/autocomplete.php
@@ -35,93 +35,110 @@ class AdminerAutocomplete
             }
 
             .shortcut-wrapper {
-            padding: 8px;
-            padding-left: 0;
+            padding-top: 8px;
             display: flex;
             gap: 8px;
+            }
+
+            .shortcut-wrapper input[type="submit"]:last-child {
+            margin-left: auto;
             }
             </style>
             <script<?php echo nonce(); ?> src="static/ace/ace.js">
                 </script>
-                <script<?php echo nonce(); ?> src="static/ace/ext-language_tools.js">
+                <script<?php echo nonce(); ?> src="static/ace/sql-formatter.js">
                     </script>
-                    <script<?php echo nonce(); ?>>
-                        document.addEventListener('DOMContentLoaded', () => {
-                        var keywords = <?php echo json_encode($this->keywords) ?>;
-                        var suggests = <?php echo json_encode($suggests) ?>;
-                        var preSQLArea = document.querySelector('pre.sqlarea');
-                        preSQLArea.hidden = true;
-
-                        var textarea = document.querySelector('textarea.sqlarea');
-                        var form = textarea.closest("form");
-
-                        const editorElem = document.createElement("div");
-                        form.appendChild(editorElem)
-                        var editor;
-
-                        ace.config.set('basePath', 'static/ace');
-                        editor = ace.edit(editorElem);
-                        editor.setTheme('ace/theme/monokai');
-                        editor.session.setMode('ace/mode/sql');
-                        editor.setOptions({
-                        fontSize: 14,
-                        enableBasicAutocompletion: [{
-                        identifierRegexps: [/[a-zA-Z_0-9\.\-\u00A2-\uFFFF]/], // added dot
-                        getCompletions: (editor, session, pos, prefix, callback) => {
-                        // note, won't fire if caret is at a word that does not have these letters
-                        callback(null, [
-                        ...keywords.map((word) => ({value: word + ' ', score: 1, meta: 'keyword'})),
-                        ...suggests.map((word) => ({value: word + ' ', score: 2, meta: 'name'}))
-                        ]);
-                        },
-                        }],
-                        // to make popup appear automatically, without explicit ctrl+space
-                        enableLiveAutocompletion: true,
-                        });
-
-                        textarea.hidden = true;
-                        form.appendChild(textarea);
-                        editor.getSession().on('change', () => {
-                        textarea.value = editor.getSession().getValue();
-                        });
-
-                        // Shortcuts buttons
-                        const buttonList = [
-                        {
-                        label: "SELECT",
-                        query: "SELECT * \nFROM \"tmp\";\n"
-                        },
-                        {
-                        label: "INSERT",
-                        query: "INSERT INTO \"tmp\" (col1, col2) \nVALUES ('val1', 'val2');\n"
-                        },
-                        {
-                        label: "UPDATE",
-                        query: "UPDATE \"tmp\" \nSET col1 = 'val1', col2 = 'val2' \nWHERE 1=1;\n"
-                        },
-                        {
-                        label: "DELETE",
-                        query: "DELETE FROM \"tmp\" \nWHERE 1=1;\n"
-                        },
-                        ]
-
-                        const shortcutWrapper = document.createElement("div");
-                        shortcutWrapper.classList.add("shortcut-wrapper");
-                        form.appendChild(shortcutWrapper)
-
-                        for(const {label, query} of buttonList) {
-                        const buttonElem = document.createElement("input")
-                        buttonElem.type = "submit";
-                        buttonElem.value = label;
-                        buttonElem.addEventListener("click", (event) => {
-                        event.preventDefault();
-                        editor.insert(query)
-                        });
-                        shortcutWrapper.appendChild(buttonElem)
-                        }
-
-                        });
+                    <script<?php echo nonce(); ?> src="static/ace/ext-language_tools.js">
                         </script>
-                <?php
+                        <script<?php echo nonce(); ?>>
+                            document.addEventListener('DOMContentLoaded', () => {
+                            const keywords = <?php echo json_encode($this->keywords) ?>;
+                            const suggests = <?php echo json_encode($suggests) ?>;
+                            const preSQLArea = document.querySelector('pre.sqlarea');
+                            preSQLArea.hidden = true;
+
+                            const textarea = document.querySelector('textarea.sqlarea');
+                            const form = textarea.closest("form");
+
+                            const editorElem = document.createElement("div");
+                            form.appendChild(editorElem)
+                            const editor = ace.edit(editorElem);
+                            editor.setTheme('ace/theme/monokai');
+                            editor.session.setMode('ace/mode/sql');
+                            editor.setOptions({
+                            fontSize: 14,
+                            enableBasicAutocompletion: [{
+                            identifierRegexps: [/[a-zA-Z_0-9\.\-\u00A2-\uFFFF]/], // added dot
+                            getCompletions: (editor, session, pos, prefix, callback) => {
+                            // note, won't fire if caret is at a word that does not have these letters
+                            callback(null, [
+                            ...keywords.map((word) => ({value: word + ' ', score: 1, meta: 'keyword'})),
+                            ...suggests.map((word) => ({value: word + ' ', score: 2, meta: 'name'}))
+                            ]);
+                            },
+                            }],
+                            // to make popup appear automatically, without explicit ctrl+space
+                            enableLiveAutocompletion: true,
+                            });
+
+                            textarea.hidden = true;
+                            form.appendChild(textarea);
+                            editor.getSession().on('change', () => {
+                            textarea.value = editor.getSession().getValue();
+                            });
+
+                            // Shortcuts buttons
+                            const buttonList = [
+                            {
+                            label: "SELECT",
+                            query: "SELECT * FROM \"tmp\";"
+                            },
+                            {
+                            label: "INSERT",
+                            query: "INSERT INTO \"tmp\" (col1, col2) VALUES ('val1', 'val2');"
+                            },
+                            {
+                            label: "UPDATE",
+                            query: "UPDATE \"tmp\" SET col1 = 'val1', col2 = 'val2' WHERE 1=1;"
+                            },
+                            {
+                            label: "DELETE",
+                            query: "DELETE FROM \"tmp\" WHERE 1=1;"
+                            },
+                            ]
+
+                            const shortcutWrapper = document.createElement("div");
+                            shortcutWrapper.classList.add("shortcut-wrapper");
+                            form.appendChild(shortcutWrapper)
+
+                            for(const {label, query} of buttonList) {
+                            const buttonElem = document.createElement("input")
+                            buttonElem.type = "submit";
+                            buttonElem.value = label;
+                            buttonElem.addEventListener("click", (event) => {
+                            event.preventDefault();
+                            editor.insert(query);
+                            const code = editor.getSession().getValue();
+                            editor.getSession().setValue(sqlFormatter.format(code, { dialect: "postgresql" }));
+                            editor.focus();
+                            });
+                            shortcutWrapper.appendChild(buttonElem)
+                            }
+
+                            const buttonElem = document.createElement("input");
+                            buttonElem.type = "submit";
+                            buttonElem.value = "Beautify";
+                            buttonElem.addEventListener("click", (event) => {
+                            event.preventDefault();
+                            // prettify
+                            const code = editor.getSession().getValue();
+                            editor.getSession().setValue(sqlFormatter.format(code, { dialect: "postgresql" }));
+                            editor.focus();
+                            });
+
+                            shortcutWrapper.appendChild(buttonElem);
+                            });
+                            </script>
+                    <?php
+                }
             }
-        }
